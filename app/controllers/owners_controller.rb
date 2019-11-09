b0ecadfbfe5daca40d660718a7bd9785f946ce58
Alouiscious Media <info@reneesoul.com>
2019-11-08 23:43:34 -0800

    Update owners_controller.rb

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -21,27 +21,27 @@ class OwnersController < ApplicationController
 
   get '/owners/:id/edit' do 
     @owner = Owner.find(params[:id])
+    # @pets = @Pet.all
     erb :'/owners/edit'
   end
 
   get '/owners/:id' do 
-    @owner = Owner.find_by_id(params[:id])
-    erb :'owners/edit'
+    @owner = Owner.find(params[:id])
+    erb :'/owners/show'
   end
 
   patch '/owners/:id' do 
+    @owner = Owner.find(params[:id])
+    @owner.update(params["owner"])
+  
     if !params[:owner].keys.include?("pet_ids")
       params[:owner]["pet_ids"]=[]
     end
 
-    @owner = Owner.find(params[:id])
-    @owner.update(params["owner"])
-    
     if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
     end
 
     redirect "owners/#{@owner.id}"
-   binding.pry
   end
 end
